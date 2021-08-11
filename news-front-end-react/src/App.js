58659002c166b59583b27a14d1b94f122f0b8b8e
Kyle Castillo <kyle.castillo90@gmail.com>
2021-08-11 16:39:47 -0400

    auth0 working for login/signup, but not preventing posts

--- news-front-end-react/src/App.js
+++ news-front-end-react/src/App.js
@@ -1,33 +1,29 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-
+import { BrowserRouter as Router } from "react-router-dom"
+import { Auth0Provider } from "@auth0/auth0-react"
+import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history"
 import Posts from './components/Posts'
-import Users from './components/Users'
-import Session from './components/Sessions'
+import Navbar from './components/nav-bar'
 
 const App = () => {
 
   // Component declarations
-  const [isAuthenticated, userHasAuthenticated] = useState(false)
   let [posts, getPosts] = useState('')
-  let [users, getUsers] = useState('')
-  let [session, getSession] = useState('')
+  let [navbar, getNavbar] = useState('')
+
 
   return(
-    <div class="container">
-      <h1 id="page-title">DISCUSS</h1>
-
-      <div id="user-session-btns">
-        <Users users={users}/>
-
-        <Session session={session}/>
+      <div class="container">
+        <h1 id="page-title">DISCUSS</h1>
+        <Router>
+          <Auth0ProviderWithHistory>
+                <Navbar navbar={navbar}/>
+          </Auth0ProviderWithHistory>
+        </Router>
+        <Posts posts={posts}/>
       </div>
-
-      <Posts posts={posts}/>
-
-    </div>
-
-  )
+    )
 }
 
 export default App
